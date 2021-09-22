0dbeff74b5a52f8e08384741b9f88dcaedebc1f2
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-09-22 16:00:11 +0700

    [add] HttpClient,regex

---
+++ src/utils/regex.js
@@ -0,0 +1,12 @@
+export const validEmail = new RegExp(
+    '^[a-zA-Z0-9._:$!%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]$'
+ );
+ export const validPassword = new RegExp('^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$');
+
+ export const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+      (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+  }
