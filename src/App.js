d42a3e0106c13d9a12e2b465c95a1714223bcdba
hypnos <ponpavee.yys@mail.kmutt.ac.th>
2021-11-22 14:21:22 +0700

    [edit]SecuredRoute

--- src/App.js
+++ src/App.js
@@ -4,25 +4,44 @@ import './App.css';
 import Login from "./components/login";
 import Information from "./components/information";
 import Header from "./components/Header/Header";
+import { server, YES} from "./constants";
 import {
   BrowserRouter as Router,
   Route,
   Redirect,
 } from "react-router-dom";
 
+const isLoggedIn = () => {
+  return localStorage.getItem(server.LOGIN_PASSED) === YES;
+  // return localStorage.getItem("Token") !== null;
+};
+
+const SecuredRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isLoggedIn() === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
 class App extends Component {
 
   redirectToLogin = () => {
     return <Redirect to="/login" />;
   };
-
+  
   render() {
     return (
       <Router>
         <div>   
         <Header/>
         <Route path="/login" component={Login} />
-        <Route path="/Information" component={Information} />
+        <SecuredRoute path="/Information" component={Information} />
         </div>
       </Router>
     );
